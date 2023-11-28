c370b574996e81093d85c4f8d9d558c705b99685
dliptai <dliptai@swin.edu.au>
2023-11-28 11:29:57 +1100

    better update.sh script

--- easybuild/easyconfigs/update.sh
+++ easybuild/easyconfigs/update.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
 local_branch="ozstar-v2"
 
-echo ">> Fetching upstream..."
-git fetch upstream
+echo ">> Fetching..."
+git fetch -a
 echo; echo ">> Checking out branch: ${local_branch} ..."
 git checkout ${local_branch}
+echo; echo ">> Pulling: origin/${local_branch} ..."
+git pull
 echo; echo ">> Merging: upstream/develop -> (local)/${local_branch} ..."
 git merge upstream/develop
 echo; echo ">> Done."
